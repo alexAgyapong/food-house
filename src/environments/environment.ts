3dc0dff90e3d0ebf5aeef7888c4133270cad92d4
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-06-07 09:29:34 +0100

    Add site nav (initial)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
-
+const API_KEY = '26d786fdd30b3a3d2281d3197b78d7dc';
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '26d786fdd30b3a3d2281d3197b78d7dc',
+  baseUrl: 'https://developers.zomato.com/api/v2.1'
 };
 
